16a4601ad1ba31f3cc6e2a973544fb6805e0b2ce
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-05-20 23:11:58 +0500

    Create StreamComparator.java

---
+++ StreamComparator.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.Comparator;
+
+public class StreamComparator implements Comparator<Stream> {
+    @Override
+    public int compare(Stream o1, Stream o2) {
+        return Integer.compare(o1.getStream().size(), o2.getStream().size());
+    }
+}
