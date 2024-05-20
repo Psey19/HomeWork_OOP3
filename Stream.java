d506421d567153acd178aecbd0b503640c00574a
Psey19 <153425698+Psey19@users.noreply.github.com>
2024-05-20 23:07:36 +0500

    Create Stream.java

---
+++ Stream.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Stream implements Iterable<StudentGroup>{
+    private List<StudentGroup> stream;
+
+    public Stream() {
+        stream = new ArrayList<>();
+    }
+
+    public List<StudentGroup> getStream() {
+        return stream;
+    }
+
+    public void addStudentGroupInStream(StudentGroup studentGroup){
+        stream.add(studentGroup);
+    }
+
+
+    @Override
+    public String toString() {
+        return "Stream{" +
+                "stream=" + stream +
+                '}';
+    }
+
+    @Override
+    public Iterator<StudentGroup> iterator() {
+        return stream.iterator();
+
+    }
+
+}
